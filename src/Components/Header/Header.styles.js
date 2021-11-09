b262241b6b5d8e77af48f7d35830d50d35e69c4f
shanks <cheikh.kebe.01@gmail.com>
2021-11-09 21:39:32 +0100

    fix header

--- src/Components/Header/Header.styles.js
+++ src/Components/Header/Header.styles.js
@@ -4,20 +4,22 @@ export const Wrapper = styled.div`
   padding: 0 20px;
   background: #ba181b;
   border-radius: 10px;
+  height: 100px;
 `;
 
 export const Content = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-around;
-  padding: 20px 0;
+  padding: 10px 0;
   margin: 0 auto;
 `;
 
 export const LogoImg = styled.img`
-  box-shadow: 5px 6px 12px  #0b090a;
-  width: 130px;
+  box-shadow: 5px 6px 11px  #0b090a;
+  width: 80px;
   border-radius: 20px;
+  background: #f5f3f4;
 
   @media screen and (max-width: 500px) {
     width: 80px;
