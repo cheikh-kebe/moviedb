6ac993b682e5794d85d890631389f7a8023acafc
shanks <cheikh.kebe.01@gmail.com>
2021-12-25 19:07:22 +0100

    fix bug

--- src/Components/Searchbar/index.jsx
+++ src/Components/Searchbar/index.jsx
@@ -1,7 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Content, Wrapper } from "./Searchbar.styles";
-//import SearchIcon from "../../images/search-icon.svg";
-import { useEffect } from "react/cjs/react.development";
 
 export const Searchbar = ({ setSearchTerm }) => {
   const [state, setState] = useState("");
@@ -17,7 +15,6 @@ export const Searchbar = ({ setSearchTerm }) => {
   return (
     <Wrapper>
       <Content>
-        {/* <img src={SearchIcon} alt="search icon" /> */}
         <input
           type="text"
           name="SearchMovie"
